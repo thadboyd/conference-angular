d51262aa2b9f35348577f5af0467b485576e218d
Thad <thad@daddyslick.com>
2015-04-21 16:58:22 -0700

    Added angular-sanitize.js and got HTML in credits.json to parse correctly. Modals still not working correctly; probably a jQuery issue.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-var conferenceApp = angular.module('conferenceApp', []);
+var conferenceApp = angular.module('conferenceApp', ['ngSanitize']);
 
 conferenceApp.controller('CategoryCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('data/categories.json').success(function(data) {
