71997a7c63c78ae2e7c7042ed2d57c3a801e74fc
Thad <thad@daddyslick.com>
2015-04-21 15:25:19 -0700

    Added footer. reveal-modal is not working, and HTML is being displayed instead of parsed.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,6 +8,10 @@ conferenceApp.controller('CategoryCtrl', ['$scope', '$http', function($scope, $h
   $http.get('data/categories.json').success(function(data) {
     $scope.categories = data;
   });
+}]);
 
-  
+conferenceApp.controller('CreditCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('data/credits.json').success(function(data) {
+    $scope.credits = data;
+  });
 }]);
