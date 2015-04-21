f80376d269956492ea55b7ad3ce7213ed8586310
Thad <thad@daddyslick.com>
2015-04-21 16:36:11 -0700

    Rolling back routing changes.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,26 +2,16 @@
 
 /* Controllers */
 
-var conferenceControllers = angular.module('conferenceControllers', []);
+var conferenceApp = angular.module('conferenceApp', []);
 
-conferenceControllers.controller('CategoryCtrl', ['$scope', '$http', function($scope, $http) {
+conferenceApp.controller('CategoryCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('data/categories.json').success(function(data) {
     $scope.categories = data;
   });
 }]);
 
-conferenceControllers.controller('CreditCtrl', ['$scope', '$http', function($scope, $http) {
+conferenceApp.controller('CreditCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('data/credits.json').success(function(data) {
     $scope.credits = data;
   });
 }]);
-
-conferenceControllers.controller('CreditPartialCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.creditFile = $routeParams.id;
-  }]);
-
-conferenceControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.phoneId = $routeParams.phoneId;
-  }]);
