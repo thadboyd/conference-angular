418c9b269b4f12efbbe797001aadbe8a2158c6ba
Thad <thad@daddyslick.com>
2015-04-30 16:02:04 -0700

    Got modal test working in footer; still need to re-implement to use my modals instead of just example modal.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -36,4 +36,58 @@ conferenceControllers.controller('CreditCtrl', ['$scope', '$http',
     $http.get('data/credits.json').success(function(data) {
       $scope.credits = data;
     });
+    $scope.open = function () {
+
+      var modalInstance = $modal.open({
+	templateUrl: 'myModalContent.html',
+	controller: 'ModalInstanceCtrl',
+	resolve: {
+	  items: function () {
+	    return $scope.items;
+	  }
+	}
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+	$scope.selected = selectedItem;
+      }, function () {
+	$log.info('Modal dismissed at: ' + new Date());
+      });
+    };
   }]);
+
+conferenceControllers.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function () {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+});
+
+conferenceControllers.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
