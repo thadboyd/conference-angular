d78dadc58c7218514acacc1608fa23eef6327b9a
Thad <thad@daddyslick.com>
2015-05-19 10:48:33 -0700

    Added framework for Karma unit testing. No tests written yet.

---
+++ test/unit/controllerSpec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+describe('Conference controllers', function() {
+  describe('ScheduleCtrl', function(){
+    var scope, ctrl, $httpBackend;
+    
+  });
+});
